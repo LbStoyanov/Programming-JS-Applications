e2886703309f48d3a83e34cb9d1716409695de85
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-31 17:37:05 +0200

    Async programming JS applications

--- Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
+++ Asynchronous Programming - Exercise/02.Bus-Schedule/app.js
@@ -1,7 +1,27 @@
 function solve() {
+    let infoDiv = document.getElementsByClassName('info')[0];
+    let url = '';
+    let departBtn = document.getElementById('depart');
+    let arriveBtn = document.getElementById('arrive');
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        url = 'http://localhost:3030/jsonstore/bus/schedule/depot';
+        infoDiv.textContent = 'Loading...';
+        const response = await fetch(url);
+
+        if (response.status !== 200) {
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+            throw new Error('Stop ID not found!')
+        }
+
+        const data = await response.json();
+
+        let stopName = data.name;
+        let nextStopID = data.next;
+       
+        departBtn.disabled = true;
+        arriveBtn.disabled = false;
     }
 
     function arrive() {
