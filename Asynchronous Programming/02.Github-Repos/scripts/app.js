9e6f58ea56665ec8a6247c474e26fdc61a9dac67
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-31 11:11:59 +0200

    GET with Fetch

--- Asynchronous Programming/02.Github-Repos/scripts/app.js
+++ Asynchronous Programming/02.Github-Repos/scripts/app.js
@@ -1,3 +1,38 @@
 function loadRepos() {
-	console.log("TODO...");
+  let username = document.getElementById("username").value;
+
+  const url = `https://api.github.com/users/${username}/repos`;
+
+  fetch(url)
+    .then(responseHandler)
+    .then(onSuccess)
+    .catch(onError);
+
+  function responseHandler(response) {
+	if (response.ok == false) {
+		throw new Error(`Error:${response.status} ${response.statusText}`);
+	}
+
+	return response.json();
+  }
+
+  function onSuccess(data) {
+	const list = document.getElementById('repos');
+
+	const items = data.map(repo => {
+		const li = document.createElement('li');
+		const a = document.createElement('a');
+		a.href = repo.html_url;
+		a.textContent = repo.full_name;
+		li.appendChild(a);
+		return li;
+	})
+
+	list.replaceChildren(...items);
+  }
+
+  function onError(error) {
+    const list = document.getElementById('repos');
+	list.textContent = error.message;
+  }
 }
