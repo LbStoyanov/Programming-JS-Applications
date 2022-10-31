2918e4d8febb4f65a5268ed971958b98f66a43a0
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-10-31 08:37:01 +0200

    HTTP requests

--- Asynchronous Programming/01.XHR/scripts/app.js
+++ Asynchronous Programming/01.XHR/scripts/app.js
@@ -1,3 +1,18 @@
 function loadRepos() {
-   console.log("TODO...");
+   let div = document.getElementById('res');
+   let loadBtn = document.getElementsByTagName('button')[0];
+
+   loadBtn.addEventListener('click',() => {
+      let request = new XMLHttpRequest();
+      let url = 'https://api.github.com/users/testnakov/repos';
+
+      request.addEventListener('readystatechange',function ()  {
+         if (request.readyState == 4 && request.status == 200) {
+            div.textContent = request.responseText;
+         }
+      });
+
+      request.open('GET',url);
+      request.send();
+   })
 }
