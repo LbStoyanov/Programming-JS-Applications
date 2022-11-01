7fb6fb3f4313521e0f23d82ca24e765b17fe9037
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-01 11:06:23 +0200

    AsyncAwaitSugerSyntax

--- Asynchronous Programming - Exercise/03.Forecaster/app.js
+++ Asynchronous Programming - Exercise/03.Forecaster/app.js
@@ -1,5 +1,35 @@
-function attachEvents() {
-    console.log("TODO...");
+ function attachEvents() {
+  const wetherIcons = {
+    Sunny: "&#x2600",
+    PartialySunny: "&#x26C5",
+    Overcast: "&#x2601",
+    Rain: "&#x2614",
+    Degrees: "&#176",
+  };
+
+  document.getElementById('submit').addEventListener('click',getWeather);
+
+  async function getWeather() {
+
+    //Barcelona
+    let cityNameInput = document.getElementById("location").value;
+    
+
+    const url = "http://localhost:3030/jsonstore/forecaster/locations";
+
+    const response = await fetch(url);
+
+    if (response.status !== 200) {
+      throw new Error("Error");
+    }
+
+    //List of objects(cityCode,cityName)
+    const listOfCitiesInfo = await response.json();
+
+    let searchedCity = listOfCitiesInfo.find(c => c.name === cityNameInput);
+    
+
+  }
 }
 
 attachEvents();
