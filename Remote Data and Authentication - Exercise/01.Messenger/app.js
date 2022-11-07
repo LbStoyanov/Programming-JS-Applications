9907f760e47dbca0d31826222fb0b60fba4c378c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-07 09:58:40 +0200

    Javascript applications

--- Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,5 +1,26 @@
 function attachEvents() {
-    console.log('TODO...');
+    const url = 'http://localhost:3030/jsonstore/messenger';
+
+    let sendBtn = document.getElementById('submit');
+    sendBtn.addEventListener('click', createMessage);
+
+    let refreshBtn = document.getElementById('refresh')
+    refreshBtn.addEventListener('click',showMessage);
+
+
+
+    function createMessage(e){
+        fetch(url,{
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}
+        })
+    }
+
+    function showMessage(e){
+        
+    }
+
+
 }
 
 attachEvents();
