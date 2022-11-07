5d7ec15f6d4aaebf64b8809ab9fcd64df465b705
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-07 11:05:34 +0200

    Javascript applications

--- Remote Data and Authentication - Exercise/01.Messenger/app.js
+++ Remote Data and Authentication - Exercise/01.Messenger/app.js
@@ -1,26 +1,38 @@
+const url = "http://localhost:3030/jsonstore/messenger";
+let textArea = document.getElementById("messages");
+
 function attachEvents() {
-    const url = 'http://localhost:3030/jsonstore/messenger';
+  let sendBtn = document.getElementById("submit");
+  sendBtn.addEventListener("click", createMessage);
 
-    let sendBtn = document.getElementById('submit');
-    sendBtn.addEventListener('click', createMessage);
+  let refreshBtn = document.getElementById("refresh");
+  refreshBtn.addEventListener("click", showMessages);
+}
 
-    let refreshBtn = document.getElementById('refresh')
-    refreshBtn.addEventListener('click',showMessage);
+function createMessage() {
+  let authorName = document.querySelector("input[name=author]").value;
+  let messageText = document.querySelector("input[name=content]").value;
 
+  let obj = {
+    author: authorName,
+    content: messageText,
+  };
 
+  fetch(url, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(obj),
+  });
+}
 
-    function createMessage(e){
-        fetch(url,{
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'}
-        })
-    }
+async function showMessages() {
+  const response = await fetch(url);
 
-    function showMessage(e){
-        
-    }
+  const data = await response.json();
 
-
+  textArea.value = Object.values(data)
+    .map(({ author, content }) => `${author}: ${content}`)
+    .join("\n");
 }
 
 attachEvents();
