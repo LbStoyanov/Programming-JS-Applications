083a93d4f020d96e2d204005b1a7124371df1414
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-07 11:59:54 +0200

    JS Applications

--- Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -1,5 +1,27 @@
+let phonebookList = document.getElementById('phonebook');
+
 function attachEvents() {
-    console.log('TODO...');
+    document.getElementById('btnLoad').addEventListener('click',loadPhonebook);
+}
+
+async function loadPhonebook(){
+    const url = `http://localhost:3030/jsonstore/phonebook`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let contacts = Object.values(data);
+    
+    for(let person of contacts){
+        let li = document.createElement('li');
+        li.textContent = `${person.person}: ${person.phone}`;
+        let button = document.createElement('button');
+        button.classList.add('button');
+        button.innerText = 'Delete';
+        li.appendChild(button);
+        phonebookList.appendChild(li);
+    }
+
 }
 
 attachEvents();
