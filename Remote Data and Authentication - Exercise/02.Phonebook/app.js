fbb499de1219ff83591d1e6f6a72acd215d9e5a5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-07 12:43:51 +0200

    JS applications

--- Remote Data and Authentication - Exercise/02.Phonebook/app.js
+++ Remote Data and Authentication - Exercise/02.Phonebook/app.js
@@ -2,6 +2,34 @@ let phonebookList = document.getElementById('phonebook');
 
 function attachEvents() {
     document.getElementById('btnLoad').addEventListener('click',loadPhonebook);
+    document.getElementById('btnCreate').addEventListener('click',createContact);
+}
+
+async function createContact(){
+    const url = `http://localhost:3030/jsonstore/phonebook`;
+
+    let personInput = document.getElementById('person');
+    let phoneInput = document.getElementById('phone');
+
+    if (personInput.value === '' || phoneInput.value === '') {
+        alert('Required fields!');
+    }
+
+    let contactObj = {
+        person: personInput.value,
+        phone: phoneInput.value
+    }
+
+    await fetch(url,{
+        method: 'POST',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify(contactObj)
+    });
+
+    debugger
+
+    personInput.value = '';
+    phoneInput.value = '';
 }
 
 async function loadPhonebook(){
@@ -13,15 +41,28 @@ async function loadPhonebook(){
     let contacts = Object.values(data);
     
     for(let person of contacts){
+
         let li = document.createElement('li');
         li.textContent = `${person.person}: ${person.phone}`;
         let button = document.createElement('button');
         button.classList.add('button');
         button.innerText = 'Delete';
+        button.addEventListener('click',deleteContact);
         li.appendChild(button);
         phonebookList.appendChild(li);
+
+        async function deleteContact(){
+
+            //const url = `http://localhost:3030/jsonstore/phonebook/${}`;
+           
+        }
     }
 
+    
+
 }
+
+
+
 
 attachEvents();
