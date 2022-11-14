d5b6e0a22c14be572d4d01eac89113c978de8b6c
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-14 14:49:40 +0200

    LIT-HTML

--- Client Side Rendering/01.List-Towns/app.js
+++ Client Side Rendering/01.List-Towns/app.js
@@ -1,6 +1,24 @@
+import { html,render } from '../node_modules/lit-html/lit-html.js';
+
 
 const root = document.getElementById('root');
 
-document.getElementById('btnLoadTowns').addEventListener('click',()=>{
+const listTemplate = (data) =>html`
+    <ul>
+        ${data.map( town => html`<li>${town}</li>`)}
+    </ul>
+`
+
+document.getElementById('btnLoadTowns').addEventListener('click',(e)=>{
+    e.preventDefault();
+   
+    if (document.getElementById('towns').value !== '') {
+        const input = document.getElementById('towns').value.split(', ');
+    
+        const result = listTemplate(input);
+        render(result,root);
+    
+        document.getElementById('towns').value = '';
+    }
     
 })
