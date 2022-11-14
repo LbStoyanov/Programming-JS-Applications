4a1912d0454535b9708408b1dc2dcaed76443d5b
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-14 23:05:53 +0200

    CSR

--- Client Side Rendering/03.Search-in-List/search.js
+++ Client Side Rendering/03.Search-in-List/search.js
@@ -1,3 +1,49 @@
+import { towns } from './towns.js';
+import { html,render } from '../node_modules/lit-html/lit-html.js';
+
+const townsRoot = document.getElementById('towns');
+const result = document.getElementById('result');
+
+
 function search() {
-   // TODO
+   
+   document.getElementsByTagName("button")[0].addEventListener('click', searchTown);
+
+   function searchTown(){
+      
+      const textNode = document.getElementById('searchText');
+      const text = textNode.value.toLowerCase();
+
+      update(text);
+      
+   }
+
+   function renderTowns(townsName, match){
+
+      const townsValue = html`
+            <ul>
+               ${townsName.map(tn => createTownTemplate(tn,match))}              
+            </ul>          
+      `
+
+      return townsValue;
+      
+    } 
+
+    function update(text){
+      const ul = renderTowns(towns,text);
+
+      render(ul,townsRoot);
+
+    }
+
+    update();
+    
+    function createTownTemplate(town, match){
+      return html`         
+              <li class="${town.toLowerCase().includes(match) ? "active" : ""}">${town}</li>
+      `
+    }
 }
+
+search();
