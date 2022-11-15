b4d7fd5088519052e221ce381b3c92fd90e56367
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-15 11:45:52 +0200

    CSR

--- Client Side Rendering/04.Fill-Dropdown/dropdown.js
+++ Client Side Rendering/04.Fill-Dropdown/dropdown.js
@@ -1,3 +1,32 @@
-function addItem() {
-    console.log('TODO:...');
+import { html,render } from '../node_modules/lit-html/lit-html.js';
+
+const url = 'http://localhost:3030/jsonstore/advanced/dropdown';
+
+let dropDownMenu = document.getElementById('menu');
+
+async function makeGetRequest(){
+
+    const response = await fetch(url);
+    const data = await response.json();
+   
+    return Object.values(data);
 }
+
+ async function initialItemsLoading(){
+
+    const initialDropDownValues =  await makeGetRequest();
+
+    const result = initialDropDownValues.map(x =>  html`<option>${x.text}</option>`);
+
+    render(result,dropDownMenu);
+
+   
+}
+initialItemsLoading();
+
+
+
+function addItem(value) {
+    
+}
+
