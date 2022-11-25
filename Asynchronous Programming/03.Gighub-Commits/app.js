b85ca764007e2fae8e191b10f0afe2dce4927879
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-25 15:35:45 +0200

    promisesJS

--- Asynchronous Programming/03.Gighub-Commits/app.js
+++ Asynchronous Programming/03.Gighub-Commits/app.js
@@ -1,4 +1,29 @@
-function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+async function loadCommits() {
+  const commits = document.getElementById("commits");
+
+  const userName = document.getElementById("username").value;
+  const repo = document.getElementById("repo").value;
+
+  const url = `https://api.github.com/repos/${userName}/${repo}/commits`;
+
+  const response = await fetch(url);
+ 
+  try {
+    if (response.status !== 200) {
+        throw new Error(response.status);
+      }
+    
+      const data = await response.json();
+    
+      Object.values(
+        data.map((r) => {
+          console.log(r);
+        })
+      );
+      
+  } catch (error) {
+    const li = document.createElement('li');
+    li.textContent = error.status;
+    commits.appendChild(li);
+  }
 }
