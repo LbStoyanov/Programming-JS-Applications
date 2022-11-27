415c7709a228b13e2f1ab42b6867a65021e9738e
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-27 15:52:41 +0200

    RemoteDataLiveDemo

--- Remote Data and Authentication/live-demo/app.js
+++ Remote Data and Authentication/live-demo/app.js
@@ -2,10 +2,21 @@ function init(){
 
 }
 
-function getComments(){
+async function getComments(){
 
+    const response = await fetch('http://localhost:3030/jsonstore/comments');
+    const data = await response.json();
+
+    return data;
 }
 
-function postComment(comment){
-    
+async function postComment(comment){
+    const response = await fetch('http://localhost:3030/jsonstore/comments',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(comment)
+
+    });
 }
