591f55d83397cf1b16a3d58e44d6a2240a21cee4
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-11-27 16:27:37 +0200

    RemoteDataLiveDemo

--- Remote Data and Authentication/live-demo/app.js
+++ Remote Data and Authentication/live-demo/app.js
@@ -1,4 +1,34 @@
 function init(){
+    document.getElementById('load').addEventListener('click', async () => {
+        const comments = await getComments();
+        displayComments(comments);
+    });
+
+    document.getElementById('send').addEventListener('click', onPost);
+}
+
+async function onPost(){
+    const name = document.querySelector('[name="name"]').value;
+    const content = document.querySelector('[name="content"]').value;
+
+    await postComment({name,content});
+
+}
+
+function displayComments(comments){
+    const list = document.getElementById('comments');
+    list.replaceChildren(...comments.map(createCommentCard))
+}
+
+function createCommentCard(comment){
+    const element = document.createElement('article');
+    ///WRONG APPROACH.Just for save time in the demo!!!
+
+    element.innerHTML = `<header><h3>${comment.name}</h3></header>
+    <main><p>${comment.content}</p></main>`;
+
+
+    return element;
 
 }
 
@@ -7,7 +37,7 @@ async function getComments(){
     const response = await fetch('http://localhost:3030/jsonstore/comments');
     const data = await response.json();
 
-    return data;
+    return Object.values(data);
 }
 
 async function postComment(comment){
@@ -19,4 +49,10 @@ async function postComment(comment){
         body: JSON.stringify(comment)
 
     });
+
+    const data = await response.json();
+
+    return data;
 }
+
+init();
