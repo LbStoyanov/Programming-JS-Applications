33972d1ad7b58a7841823b1c8f918a56a42bd0d5
Lyubomir Stoyanov <lbstoyanov1@gmail.com>
2022-12-01 15:51:43 +0200

    Spa demo

--- Single Page Applications/spa-live-demo/app.js
+++
@@ -1,4 +0,0 @@
-import {product,sum} from './utils.js';
-
-console.log(product(2,2));
-console.log(sum(2,2));
---
+++ Single Page Applications/spa-live-demo/src/catalog.js
@@ -0,0 +1,37 @@
+
+export async function showCatalogView(){
+    [...document.querySelectorAll('section')].forEach(s => s.style.display = 'none');
+    document.getElementById('catalog-page').style.display = 'block';
+
+    const recipes = await getAllRecipes();
+    displayRecipes(recipes);
+}
+
+
+ async function getAllRecipes(){
+    const response = await fetch('http://localhost:3030/data/recipes?select=' + encodeURIComponent('_id,name'));
+    const recipes = await response.json();
+
+    return recipes;
+}
+
+ function displayRecipes(recipes){
+    const cards = recipes.map(createRecipeCard);
+
+    const fragment = document.createDocumentFragment();
+
+    for(let item of cards){
+        fragment.appendChild(item);
+    }
+
+    const list = document.getElementById('recipes-list');
+    list.replaceChildren(fragment);
+}
+
+
+function createRecipeCard(recipe){
+    const element = document.createElement('li');
+    element.textContent = recipe.name;
+
+    return element;
+}
